0f3da159c4d74fd8bea442aa105dc6f937ec23f3
TuanANg <tuan15096@gmail.com>
2019-09-04 23:08:44 +0200

    update 20190904

--- solutions/114.py
+++ solutions/114.py
@@ -20,15 +20,15 @@ def reverseWords(string, delim):
 # output: a string s.t reverse the words in string while keep the relative order of delimiters
 # running time: O(n)
     # instance variables
-    marker = 0  # all the element before this index has been checked
+    marker = 0  # all the elements before this index has been checked
     pivot = 0   # keep increasing to check the char in input string
     delimiterFlag = False   # delimiter flag to check the current sequence of characters is word or delimiter
     words = []  # list of words from input string
     delimiters = [] # list of delimiters from input string
-    start = True if string[0] not in delim else False  # used to identify the order of merging
+    startByWord = True if string[0] not in delim else False  # to identify the order of merging: word if True, delim if False
     i_words = 0 # iterator in list words
     i_delimiters = 0    # iterator in list delimiters
-    output = []
+    output = [] # list of reversed words & relative-ordered delimiters
     # separate words and delimiters in order from input string
     while pivot < len(string):
         if (not delimiterFlag) and (string[pivot] not in delim):   # continue sequence of chars that makes up a word
@@ -36,10 +36,12 @@ def reverseWords(string, delim):
         elif (not delimiterFlag) and (string[pivot] in delim): # end of current word, start of new delimiter
             words.append(string[marker:pivot])  # add new word into list words
             marker = pivot  # update marker
+            pivot += 1
             delimiterFlag = True    # update flag
         elif (delimiterFlag) and (string[pivot] not in delim): # end of current delimiter, start of new word
             delimiters.append(string[marker:pivot]) # add new delimiter into list delimiters
             marker = pivot  # update marker
+            pivot += 1
             delimiterFlag = False   # update flag
         else:   # continue sequence of chars that makes up a delimiter
             pivot += 1
@@ -52,14 +54,14 @@ def reverseWords(string, delim):
     words = words[::-1]
     # merge reversed words and delimiters while maintaining the relative order of the delimiters.
     while (i_words < len(words)) and (i_delimiters < len(delimiters)):
-        if start:
-            output.append(words[i_words])
-            i_words += 1
-            start = False
+        if startByWord:
+            output.append(words[i_words])   # add word to output
+            i_words += 1    # move iterator
+            startByWord = not startByWord   # reverse to add delimiter at next round
         else:
-            output.append(delimiters[i_delimiters])
-            i_delimiters += 1
-            start = True
+            output.append(delimiters[i_delimiters]) # add delimiter to output
+            i_delimiters += 1   # move iterator
+            startByWord = not startByWord   # reverse to add word at next round
     # copy the rest to output
     while i_words < len(words):
         output.append(words[i_words])
@@ -78,3 +80,4 @@ if __name__ == "__main__":
     reverseWords_test("hello/world:here", DELIMITERS)    # print "here/world:hello"
     reverseWords_test("hello/world:here/", DELIMITERS)    # print "here/world:hello/"
     reverseWords_test("hello//world:here", DELIMITERS)    # print "here//world:hello"
+    reverseWords_test("/abc//ijk:xyz:", DELIMITERS)    # print "/xyz//ijk:abc:"
